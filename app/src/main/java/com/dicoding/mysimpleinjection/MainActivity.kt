a9052be0b2f6fa8181311287a60ddb95989693f5
Arif <faizin.arief@gmail.com>
2020-06-01 11:37:51 +0700

    Add : Dagger

--- app/src/main/java/com/dicoding/mysimpleinjection/MainActivity.kt
+++ app/src/main/java/com/dicoding/mysimpleinjection/MainActivity.kt
@@ -1,30 +1,63 @@
 package com.dicoding.mysimpleinjection
 
+import android.app.Application
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Module
+class AppModule {
+    @Provides
+    fun provideEngine(context: Context): Engine = Engine(context)
+}
+
+@Singleton
+@Component(modules = [AppModule::class])
+interface AppComponent {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun inject(activity: MainActivity)
+}
+
+open class MyApplication : Application(){
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create(
+            applicationContext
+        )    }
+}
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var engine: Engine
+    @Inject
+    lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        (application as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        engine = Engine()
-        val car = Car(engine)
         car.start()
     }
 }
 
-class Car (private val engine: Engine) {
+@Singleton
+class Car @Inject constructor(private val engine: Engine) {
     fun start() {
         engine.start()
     }
 }
 
-class Engine() {
+class Engine(val context: Context) {
     fun start() {
-        println("Engine started....")
+        println(context.applicationContext.getString(R.string.engine_start))
     }
 }
